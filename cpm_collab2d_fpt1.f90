6e24c9241714fac8954c8b7e7feda27277648314
Julien <upquark@protonmail.ch>
2015-06-22 09:37:28 -0400

    15/06/22 Added the statement:
    elseif( d <= -1.0*df )then
        firstpass(nRun) = 0.0
        tMCS = tmax
    endif
    
    This is to cut down time spent on runs that will most likely fail
    at passing FPT criteria.

--- cpm_collab2d_fpt1.f90
+++ cpm_collab2d_fpt1.f90
@@ -202,6 +202,9 @@ do while( tMCS < tmax )
         if( d >= df )then
             firstpass(nRun) = tMCS - 1
             tMCS = tmax
+        elseif( d <= -1.0*df )then
+            firstpass(nRun) = 0.0
+            tMCS = tmax
         endif
 
     endif
